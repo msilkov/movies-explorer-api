4fa124e1c817e7d9335042b58485b6089474e43b
Michael <m-silkov@inbox.ru>
2023-03-07 22:47:23 +0300

    feat: create celebrate req validation

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,13 @@
 const authRouter = require('express').Router();
 
 const { createUser, login, logout } = require('../controllers/users');
+const {
+  signInValidation,
+  signUpValidation,
+} = require('../middlewares/requetsValidation');
 
-authRouter.post('/signup', createUser);
-authRouter.post('/signin', login);
+authRouter.post('/signup', signUpValidation, createUser);
+authRouter.post('/signin', signInValidation, login);
 authRouter.post('/signout', logout);
 
 module.exports = authRouter;
