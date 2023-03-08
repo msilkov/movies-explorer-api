a1fabcca871cfa81780ea637cc63f1754355d188
Michael <m-silkov@inbox.ru>
2023-03-08 18:15:58 +0300

    feat: add helmet limiter logger

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 
 const router = require('./routes/index');
 
 const { errorsHandler } = require('./middlewares/errorsHandler');
+const { limiter } = require('./middlewares/expressLimiter');
 const { MONGO_DB_LINK } = require('./utils/constants');
 
 const app = express();
@@ -14,6 +16,8 @@ const app = express();
 mongoose.set('strictQuery', true);
 mongoose.connect(MONGO_DB_LINK);
 
+app.use(limiter);
+app.use(helmet());
 app.use(express.json());
 
 app.use(cookieParser());
